7c9dea94d9bfac77e60dbfd8c1457ad744c40734
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-03-17 11:18:08 +0100

    Changes

--- Analysis.py
+++ Analysis.py
@@ -7,5 +7,4 @@ credit_records = pd.read_csv("credit_records.csv")
 # Display the first five rows of credit_records using the .head() method
 print(credit_records.head())
 
-
 #had to stop the project here in order to fully make it on a private one :'( 
