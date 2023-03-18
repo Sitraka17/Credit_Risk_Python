58de1679caf790a738b06e5246177140e99fa291
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-03-18 23:44:54 +0100

    dsds

--- Crédit_Suisse/App.py
+++ Crédit_Suisse/App.py
@@ -1,51 +1,8 @@
-import streamlit as st
-import yfinance as yf
 import pandas as pd
-import cufflinks as cf
-import datetime
-#pip install yfinance
+import numpy as np
+import streamlit as st
 
-# App title
-st.markdown('''
-# Stock Price App
-Shown are the stock price data for query companies!
-**Credits**
-- App built by UR MOM
-- Built in `Python` using `streamlit`,`yfinance`, `cufflinks`, `pandas` and `datetime`
-''')
-st.write('---')
-
-# Sidebar
-st.sidebar.subheader('Query parameters')
-start_date = st.sidebar.date_input("Start date", datetime.date(2019, 1, 1))
-end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
-
-# Retrieving tickers data
-tickerSymbol = 'CS'
-tickerData = yf.Ticker(tickerSymbol) # Get ticker data
-tickerDf = tickerData.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
-
-# Ticker information
-string_logo = '<img src=%s>' % tickerData.info['logo_url']
-st.markdown(string_logo, unsafe_allow_html=True)
-
-string_name = tickerData.info['longName']
-st.header('**%s**' % string_name)
-
-string_summary = tickerData.info['longBusinessSummary']
-st.info(string_summary)
-
-# Ticker data
-st.header('**Ticker data**')
-st.write(tickerDf)
-
-# Bollinger bands
-st.header('**Bollinger Bands**')
-qf=cf.QuantFig(tickerDf,title='First Quant Figure',legend='top',name='CS')
-qf.add_bollinger_bands()
-fig = qf.iplot(asFigure=True)
-st.plotly_chart(fig)
-
-####
-#st.write('---')
-#st.write(tickerData.info)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
