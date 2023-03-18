a7594faa0e5b0496a8f0d083d81fd289c58e72e8
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-03-18 22:50:07 +0100

    update

--- Crédit_Suisse/App.py
+++ Crédit_Suisse/App.py
@@ -1,4 +1,3 @@
-#Try to do a streamlit app ? 
 import streamlit as st
 import yfinance as yf
 import matplotlib.pyplot as plt
@@ -6,16 +5,20 @@ import matplotlib.pyplot as plt
 # Set page title
 st.set_page_config(page_title="Credit Suisse Stock Price Evolution")
 
+def plot_stock_price_evolution(symbol):
+    # Get stock data from Yahoo Finance
+    data = yf.download(symbol, period='max')
+
+    # Plot stock price evolution
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(data['Close'])
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    ax.set_title(f'{symbol} Stock Price Evolution')
+    st.pyplot(fig)
+
 # Define stock symbol
 symbol = 'CS'
 
-# Get stock data from Yahoo Finance
-data = yf.download(symbol, period='max')
-
-# Plot stock price evolution
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(data['Close'])
-ax.set_xlabel('Date')
-ax.set_ylabel('Price')
-ax.set_title(f'{symbol} Stock Price Evolution')
-st.pyplot(fig)
+# Call the function to plot the stock price evolution
+plot_stock_price_evolution(symbol)
