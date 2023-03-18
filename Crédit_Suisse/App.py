a90eed716d0363eb7bec55ff166c8ba94a77f052
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-03-18 23:04:55 +0100

    fsds

--- Crédit_Suisse/App.py
+++ Crédit_Suisse/App.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 class Company:
     def __init__(self, ticker):
-        price_df = si.get_data(ticker, dt.datetime.now()-           dt.timedelta(days=2*365), dt.datetime.date(dt.datetime.now()))
+        price_df = si.get_data(ticker, dt.datetime.now()- dt.timedelta(days=2*365), dt.datetime.date(dt.datetime.now()))
         overview_df = si.get_stats(ticker)
         overview_df = overview_df.set_index('Attribute')
         overview_dict = si.get_quote_table(ticker)
@@ -45,3 +45,29 @@ def get_profit_margins(self):
         self.profit_margin_dict = {
             'Values' : [self.gross_margin, self.operating_margin, self.net_margin]
             }
+        
+st.title('Financial Dashboard')
+ticker_input = st.text_input('Please enter your company ticker:')
+search_button = st.button('Search')
+
+if search_button:
+    company = Company(ticker_input)
+    company.get_overview()
+    company.get_profit_margins()
+    company.get_liquidity_ratios()
+    company.get_leverage_ratios()
+    company.get_efficiency_ratios()
+
+st.header('Company overview')
+overview_index = ['Enterprise value', 'Market cap', 'EV/sales ratio', 'P/E ratio']
+overview_df = pd.DataFrame(company.overview_dict, index = overview_index)
+st.line_chart(company.prices)
+st.table(overview_df)
+
+with st.beta_expander('Profit margins (as of {})'.format(company.year_end)):
+        profit_margin_index = ['Gross margin', 'Operating margin', 'Net margin']
+        profit_margin_df = pd.DataFrame(company.profit_margin_dict, index = profit_margin_index)
+        st.table(profit_margin_df)
+        st.bar_chart(profit_margin_df)
+
+
