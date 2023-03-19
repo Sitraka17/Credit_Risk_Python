40e594d89c43c4d5a5d14ef38b7309e9f3c3b632
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-03-19 21:59:09 +0100

    matplotlib

--- credit_suisse/App.py
+++ credit_suisse/App.py
@@ -2,13 +2,11 @@ import pandas as pd
 import numpy as np
 #!pip install yfinance
 import streamlit as st
-import yfinance as finance
+#import yfinance as finance
 #chart_data = pd.DataFrame(
 #    np.random.randn(20, 3),
 #    columns=['a', 'b', 'c'])
 #st.line_chart(chart_data)
-
-
 import streamlit as st
 import matplotlib.pyplot as plt
 
